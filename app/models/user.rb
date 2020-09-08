f5d8a8d513687da33d80c07052a46bafbbd8dd1e
Becca <rrdoukas@gmail.com>
2020-09-08 16:48:52 -0500

    removes messages and ads when user deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
- has_many :ads
+ has_many :ads, dependent: :destroy
 
   # any conversations where the user is the sender OR recipient. Replaces has_many association with custom method
   def conversations
