095ded529ac4b5e7c3deb8edc63c86b4458274e3
Becca <rrdoukas@gmail.com>
2020-10-09 16:46:05 -0500

    adds url to cors

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 /* global axios */
 console.log("Sanity Check");
 
-axios.get("http://localhost:3000/api/ads").then(function(response) {
+axios.get("https://localhost:3000/api/ads").then(function (response) {
   var ads = response.data;
   console.log(ads);
 });
