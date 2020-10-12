88900d5fa2c81398657c88cf262eedba14b22a43
Becca <rrdoukas@gmail.com>
2020-10-12 12:19:07 -0500

    Updates cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins  'https://chi-plant-swapp.netlify.app/'
+    origins 'localhost:8080', 'https://chi-plant-swapp.netlify.app/
 
     resource '*',
       headers: :any,
