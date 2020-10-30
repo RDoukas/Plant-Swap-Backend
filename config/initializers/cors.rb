1d615cfe29aee363e4c6044379dee4e4a7511ec8
Becca <rrdoukas@gmail.com>
2020-10-30 11:29:39 -0500

    adds netlify url to cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'https://chi-plant.herokuapp.com/'
+    origins 'localhost:8080', 'https://modest-joliot-7ea924.netlify.app/'
 
     resource '*',
       headers: :any,
